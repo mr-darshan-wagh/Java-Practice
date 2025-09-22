d963ef6e9b82f09816c039a955de3c9bcb79859a
Darshan Wagh <waghdarshan9966@gmail.com>
2025-09-22 02:29:42 -0700

    Hello

---
+++ Hello.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Hello{
+    public static void main(String args[]){
+        System.out.println("Hii");
+
+        System.out.println("Hello KP");
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        System.out.println(a+ " Answer");
+
+
+        
+    }
+}
