e8b767ed23fff966fa12535ece77f41395ea3adb
Darshan Wagh <waghdarshan9966@gmail.com>
2025-09-30 12:18:33 -0700

    new code

---
+++ LinearSearch.java
@@ -0,0 +1,23 @@
+public class LinearSearch {
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i; // return index if found
+            }
+        }
+        return -1; // return -1 if not found
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+        int target = 30;
+
+        int result = linearSearch(numbers, target);
+
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+}
