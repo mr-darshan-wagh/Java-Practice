06f56cbe1ea17fc66a1b1e52cfc2f2fde89b5691
Darshan Wagh <waghdarshan9966@gmail.com>
2025-10-02 11:40:43 -0700

    new code

---
+++ merge1.java
@@ -0,0 +1,65 @@
+public class merge1 {
+
+    // Definition for singly-linked list
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    // Merge function
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode current = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        if (list1 != null) current.next = list1;
+        else current.next = list2;
+
+        return dummy.next;
+    }
+
+    // Helper method to build a linked list from array
+    public static ListNode buildList(int[] arr) {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        for (int num : arr) {
+            current.next = new ListNode(num);
+            current = current.next;
+        }
+        return dummy.next;
+    }
+
+    // Helper method to print linked list
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val);
+            if (head.next != null) System.out.print(" -> ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    // Main method to test
+    public static void main(String[] args) {
+        merge1 solution = new merge1();
+
+        ListNode list1 = buildList(new int[]{1, 2, 4});
+        ListNode list2 = buildList(new int[]{1, 3, 4});
+
+        ListNode merged = solution.mergeTwoLists(list1, list2);
+        printList(merged);  // Output: 1 -> 1 -> 2 -> 3 -> 4 -> 4
+    }
+}
